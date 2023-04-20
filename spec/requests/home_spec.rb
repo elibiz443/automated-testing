d385a05e134a1ba1bfd8eb9d6cbe565ce25ea41d
elibiz443 <ellyambet10@gmail.com>
2023-04-20 03:17:08 +0300

    Added home controller test

--- spec/requests/home_spec.rb
+++ spec/requests/home_spec.rb
@@ -1,6 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe "Homes", type: :request do
-  describe "GET /index" do
+  let(:auth_token) { FactoryBot.create(:auth_token) }
+  let!(:token) { { "Authorization" => "Bearer #{ auth_token.token_digest }" } }
+  
+  describe "GET #index" do
+    it "returns a success response" do
+      get "/api/v2/users", headers: token
+      expect(response).to have_http_status(:ok)
+    end
   end
 end
